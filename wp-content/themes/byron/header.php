b5ef0479b88fcff33181c88323a70b2300ce6e37
Gustavo <gurezende27@outlook.com>
2019-10-25 00:56:59 -0300

    Responsivo

--- wp-content/themes/byron/header.php
+++ wp-content/themes/byron/header.php
@@ -35,26 +35,19 @@ if (class_exists('Redux')) {
 </head>
 <body>
     <header>
-        <nav>
-            <div class="nav-wrapper">
-                <img href="#" class="brand-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_grupo_wz.png"></img>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a class="nb">HOME</a></li>
-                <li><a class="nb">SERVIÇOS</a></li>
-                <li><a class="nb">A EMPRESA</a></li>
-                <li><a class="nb">CLIENTES</a></li>
-                <li><a class="nb">CONTATO</a></li>
-                <li><a class="nb">BLOG</a></li>
-            </ul>
-        </div>
-    </nav>
-
-                <ul class="sidenav" id="mobile-demo">
-                    <li><a class="sidenav-close nb"><p>HOME</p></a></li>                   
-                    <li><a class="sidenav-close nb"><p>SERVIÇOS</p></a></li>
-                    <li><a class="sidenav-close nb"><p>A EMPRESA</p></a></li>
-                    <li><a class="sidenav-close nb"><p>CLIENTES</p></a></li>
-                    <li><a class="sidenav-close nb"><p>CONTATO</p></a></li>
-                    <li><a class="sidenav-close nb"><p>BLOG</p></a></li>
-            </ul>
-</header>
+          <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper">
+                    <img href="#" class="brand-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_grupo_wz.png"></img>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a class="nb">HOME</a></li>
+                        <li><a class="nb">SERVIÇOS</a></li>
+                        <li><a class="nb">A EMPRESA</a></li>
+                        <li><a class="nb">CLIENTES</a></li>
+                        <li><a class="nb">CONTATO</a></li>
+                        <li><a class="nb">BLOG</a></li>
+                    </ul>
+                </div>
+        </nav>
+    </div>
+    </header>
